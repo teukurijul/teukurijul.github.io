e30eb52f8969062e8db5da88414b645de4e63f1e
teukurijul <teukurijulzaman@gmail.com>
2026-08-02 21:48:42 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,19 @@
-const CACHE_NAME = 'rizu-zaman-v1';
-const ASSETS = [
+const CACHE_NAME = 'cyber-portfolio-v1';
+const urlsToCache = [
   '/',
   '/index.html',
-  '/site.webmanifest',
-  '/robots.txt',
-  '/sitemap.xml'
+  '/styles.css',
+  '/main.js'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
